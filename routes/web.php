a2ffd2cf8e02a9fb372a6a7e9d76686a37d29cbb
EuniceDael <dael.keceleunice@gmail.com>
2026-05-08 00:05:10 +0800

    fixed the routes for all the btns

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('home');
+
+Route::get('/otp-phone', function () {
+    return view('otp-phone');
+})->name('otp.phone');
+
+Route::get('/otp-email', function () {
+    return view('otp-email');
+})->name('otp.email');
+
+Route::get('/validate-otp', function () {
+    return view('validate-otp');
+})->name('validate.otp');
+
+Route::get('/mailbox', function () {
+    return view('mailbox');
+})->name('mailbox');
+
+Route::get('/ai-chatbot', function () {
+    return view('ai-chatbot');
+})->name('ai.chatbot');
